86a6773b4ae582e40cca312c9d0b766ab8f50f31
sebastienPoussard <sebastienPoussard>
2021-12-03 17:42:28 +0100

    starting with defaule colemak-dh

--- Sephiz/rules.mk
+++ Sephiz/rules.mk
@@ -1,5 +1,4 @@
-BOOTLOADER = atmel-dfu
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
-# ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
