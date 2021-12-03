d53bb85052163fa3fd6886ded7ed47b7da4d2f93
sebastienPoussard <sebastienPoussard>
2021-12-03 15:58:44 +0100

    initial commit

---
+++ Sephiz/rules.mk
@@ -0,0 +1,5 @@
+BOOTLOADER = atmel-dfu
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+# ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
