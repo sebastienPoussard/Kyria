777559177332318e276d12e54545093348666c37
sebastienPoussard <sebastienPoussard>
2021-12-07 15:10:15 +0100

    mouse scrolls up and down

--- Sephiz/rules.mk
+++ Sephiz/rules.mk
@@ -2,3 +2,4 @@ OLED_ENABLE = no
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+MOUSEKEY_ENABLE = yes   # for scrolling up and down
